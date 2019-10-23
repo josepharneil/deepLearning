94146c1f7f150f775239a6fdd6cdb7b3db24bb96
Daniel <daniel.a.scott97@gmail.com>
2019-10-23 16:53:55 +0100

    attempted import dataset

--- train.py
+++ train.py
@@ -8,3 +8,14 @@ from torch.utils.data import DataLoader
 from torch.utils.tensorboard import SummaryWriter
 from torchvision import transforms
 
+from dataset import UrbanSound8KDataset
+
+train_loader = torch.utils.data.DataLoader(
+      UrbanSound8KDataset('UrbanSound8K_train.pkl', 'LMC'),
+      batch_size=32, shuffle=True,
+      num_workers=8, pin_memory=True)
+
+val_loader = torch.utils.data.DataLoader(
+     UrbanSound8KDataset('UrbanSound8K_test.pkl', 'LMC'),
+     batch_size=32, shuffle=False,
+     num_workers=8, pin_memory=True)
