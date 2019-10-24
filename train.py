a16a7c271c9a8dedb5de4aa613128b290da615de
Joseph Arneil <josepharneil@gmail.com>
2019-10-24 14:36:09 +0100

    first structure of network

--- train.py
+++ train.py
@@ -13,9 +13,9 @@ from dataset import UrbanSound8KDataset
 device = torch.device('cuda:0' if torch.cuda.is_available() else "cpu")
 
 train_loader_LMC = torch.utils.data.DataLoader(
-      UrbanSound8KDataset('UrbanSound8K_train.pkl', 'LMC'),
-      batch_size=32, shuffle=True,
-      num_workers=8, pin_memory=True)
+    UrbanSound8KDataset('UrbanSound8K_train.pkl', 'LMC'),
+    batch_size=32, shuffle=True,
+    num_workers=8, pin_memory=True)
 
 # test_loader_LMC = torch.utils.data.DataLoader(
 #      UrbanSound8KDataset('UrbanSound8K_test.pkl', 'LMC'),
@@ -47,24 +47,118 @@ class LMC_Net(nn.Module):
     def __init__(self):
         super().__init__()
         #define layers here
+
+        ##1st layer
         self.conv1 = nn.Conv2d(
             #presuming 1 channel input image?????
             in_channels=1,
             out_channels=32,
             kernel_size=(3,3),
-            stride=(2,2)
+            #stride=(2,2)
+            padding = 1  ##BULLSHIT
         )
 
+        ##### randomly initialise params??????
 
+        self.norm2 = nn.BatchNorm2d(num_features=32)
+        
+        #relu
+
+        ##2nd layer
+        self.dropout3 = nn.Dropout(p=0.5)
+
+        self.conv4 = nn.Conv2d(
+            in_channels = 32,
+            out_channels = 32,
+            kernel_size = (3,3),
+            #stride = (2,2)
+            padding = 2  ##BULLSHIT
+        )
+
+        self.norm5 = nn.BatchNorm2d(num_features = 32)
+        
+        #relu
+
+        #maxpooling
+        self.pool6 = nn.MaxPool2d(kernel_size=(2,2))
+
+        ##3rd layer
+        self.conv7 = nn.Conv2d(
+            in_channels = 32,
+            out_channels = 64,
+            kernel_size = (3,3),
+            #stride = (2,2)
+            padding = 2  ##BULLSHIT
+        )
+
+        self.norm8 = nn.BatchNorm2d(num_features = 64)
+
+        #relu
+
+        ##4th layer
+        self.dropout9 = nn.Dropout(p=0.5)
+
+        self.conv10 = nn.Conv2d(
+            in_channels = 64,
+            out_channels = 64,
+            kernel_size = (3,3),
+            #stride = (2,2)
+            padding = 0  ##BULLSHIT
+        )
+        
+        self.norm11 = nn.BatchNorm2d(num_features = 64)
+
+        #relu
+
+        #[32, 64, 43, 21]
+        ##5th layer
+        self.fc12 = nn.Linear(in_features=57792,out_features=1024)
+        #Sigmoid
+        
+        #6th layer
+        self.dropout13 = nn.Dropout(p=0.5)
+        self.fc14 = nn.Linear(in_features=1024 ,out_features=10  )
+        #Softmax
 
     def forward(self,x):
         #define forward pass here
+        ##1
         x = self.conv1(x)
+        x = self.norm2(x)
+        x = F.relu(x)
+        ##2
+        x = self.dropout3(x)
+        x = self.conv4(x)
+        x = self.norm5(x)
+        x = F.relu(x)
+        x = self.pool6(x)
+        ##3
+        x = self.conv7(x)
+        x = self.norm8(x)
+        x = F.relu(x)
+        ##4
+        x = self.dropout9(x)
+        x = self.conv10(x)
+        x = self.norm11(x)
+        x = F.relu(x)
+
+        #Flatten
+        x = torch.flatten(x,start_dim = 1)
+
+        ##5
+        x = self.fc12(x)
+        x = torch.sigmoid(x)
+        ##6
+        x = self.dropout13(x)
+        x = self.fc14(x)
+        x = F.softmax(x,dim=1)
         return x
 
 
 
-
+###optimizer: SGD; L2 reg; learn rate 0.001; momentum 0.9
+###loss function: cross entropy
+#nb: don't use dropout in testing
 
 
 model = LMC_Net()
@@ -76,3 +170,9 @@ print(model(item[0]).shape)
 #to instantiate: LMC_model = LMC_Net()
 #to do forward pass: output = LMC_model(input)
 
+
+
+#####QUESTIONS:
+# 1.) stride and padding for the convolutional layers: e.g. how is the image size maintained across the 
+# 2.) check location of dropout layers
+# 3.) fully connected layers? paper says one layer, seems to be 2? if 1 layer, how can the output of conv layers be flatened to 1024????
